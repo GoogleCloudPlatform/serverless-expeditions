14ca5c7680b5353762d5741ef8ba94cedc662347
Roger Martinez <rgmartinez@google.com>
2020-11-19 19:22:02 -0800

    database connection and get function

--- cloud-sql-rest-api/index.js
+++ cloud-sql-rest-api/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const mysql = require("mysql");
+
 const app = express();
 app.use(express.json());
 const port = process.env.PORT || 8080;
@@ -6,6 +8,24 @@ app.listen(port, () => {
   console.log(`BarkBark Rest API listening on port ${port}`);
 });
 
+const pool = mysql.createPool({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  socketPath: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`,
+});
+
 app.get("/", async (req, res) => {
   res.json({ status: "Bark bark! Ready to roll!" });
+});
+
+app.get("/:breed", async (req, res) => {
+  const breed = req.params.breed;
+  const query = `SELECT * FROM breeds WHERE name = '${breed}'`;
+  pool.query(query, (error, results) => {
+    if (!results[0]) {
+      res.json({ status: "Not found!" });
+    }
+    res.json(results[0]);
+  });
 });
