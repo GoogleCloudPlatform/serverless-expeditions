aab93ee5cfeda4f767d770cefc8bd5c234ef176f
Roger Martinez <31829545+rogerthatdev@users.noreply.github.com>
2020-11-24 15:54:28 -0800

    Update index.js
    minor changes to variables

--- cloud-sql-rest-api/index.js
+++ cloud-sql-rest-api/index.js
@@ -8,13 +8,6 @@ app.listen(port, () => {
   console.log(`BarkBark Rest API listening on port ${port}`);
 });
 
-const pool = mysql.createPool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  socketPath: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`,
-});
-
 app.get("/", async (req, res) => {
   res.json({ status: "Bark bark! Ready to roll!" });
 });
@@ -31,19 +24,20 @@ app.get("/:breed", async (req, res) => {
   });
 });
 
+const pool = mysql.createPool({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  socketPath: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`,
+});
+
 app.post("/", async (req, res) => {
-  const data = {
-    name: req.body.name,
-    type: req.body.type,
-    lifeExpectancy: req.body.lifeExpectancy,
-    origin: req.body.origin,
-  };
   const query = `
     INSERT INTO breeds VALUES 
-    ('${data.name}',
-    '${data.type}',
-    ${data.lifeExpectancy},
-    '${data.origin}')`;
+    ('${req.body.name}',
+    '${req.body.type}',
+    ${req.body.lifeExpectancy},
+    '${req.body.origin}')`;
   pool.query(query, (error) => {
     if (error) {
       res.json({ status: "failure", reason: error.code  });
