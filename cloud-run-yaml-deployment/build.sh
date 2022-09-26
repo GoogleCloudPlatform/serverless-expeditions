c8db1006c894702df938076c7989d63a9fe9df80
rogerthatdev <roger.that.dev@gmail.com>
2022-09-26 08:29:17 -0700

    oops fix region env var

--- cloud-run-yaml-deployment/build.sh
+++ cloud-run-yaml-deployment/build.sh
@@ -1,6 +1,6 @@
 AR_REGION=us-central1
 PROJECT_ID=  # YOUR GCP PROJECT ID GOES HERE
-AR_REPO_URL=$REGION-docker.pkg.dev/$PROJECT_ID/my-serverless-app  
+AR_REPO_URL=$AR_REGION-docker.pkg.dev/$PROJECT_ID/my-serverless-app  
 
 gcloud builds submit \
   --tag $AR_REPO_URL/website \
